48e4075509c52a97ece4a9fa5bf26323a18282fe
Nikita Patrakeev <coldrain4@rambler.ru>
2023-07-23 18:53:46 +0300

    register, sign hbs

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
 const hbs = require('hbs');
 const mysql = require('mysql2');
+const session = require('express-session');
 
 app.set('views','views');
 app.set('view engine','hbs');
+
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
 
 // Назначение серверу порта и запуск сервера
 const port = 3000;
@@ -33,18 +41,55 @@ connection.connect((err) => {
 let users_arr = [];
 // Обработчик маршрута для GET-запроса на корневой маршрут
 app.get('/', (req, res) => {
-    let sql = 'SELECT * FROM users';
-    connection.query(sql, (err, result) => { 
 
-      console.log(result);
-      users_arr = result; 
-    });
+  let sql = 'SELECT * FROM users';
+  connection.query(sql, (err, result) => { 
+    users_arr = result; 
+  });
 
-    res.render(`index`, {
-      users_arr: users_arr
-    });
+  res.render(`index`, {
+    users_arr: users_arr,
+  });
+
+  console.log(req.session);
+});
+//Страница авторизации
+app.get('/sign', (req, res) => {
+  res.render(`sign`);
 });
 
+app.get('/register', (req, res) => {
+  res.render(`register`);
+});
+
+
+
+//проверка правильности введенного пароля 
+app.post('/sign', (req, res) => { 
+  let { login, password } = req.body;
+  let sql = 'SELECT * FROM users WHERE login = ? AND password = ?'; 
+  let values = [login, password];
+
+  connection.query(sql, values, (err, result) => {
+    // if (err) throw err; 
+
+    // res.send(result.length > 0); 
+
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else if (result.length === 0) {
+      res.sendStatus(401); // Неавторизованный доступ
+    } else {
+      const user = result[0];
+
+      // Сохранение информации о пользователе в сессии
+      req.session.user = user;
+      res.sendStatus(200); // Успешная авторизация
+    }
+    
+  }); 
+});
 
 
 //Для пользователей
@@ -60,28 +105,19 @@ app.post('/register', (req, res) => {
   }); 
 });
 
-//проверка правильности введенного пароля 
-app.post('/login', (req, res) => { 
-  let { login, password } = req.body;
-  let sql = 'SELECT * FROM users WHERE email = ? AND password = ?'; 
-  let values = [login, password];
-
-  connection.query(sql, values, (err, result) => { 
-    if (err) throw err; 
-    res.send(result.length > 0); 
-  }); 
+app.get('/profile', (req, res) => {
+  const user = req.session.user; // Получение информации о пользователе из объекта сессии
+  if (user) {
+    // Действия, которые вы хотите выполнить с информацией о пользователе
+    res.send(`Привет, ${user.username}! Ваш email: ${user.email}`);
+  } else {
+    res.sendStatus(401); // Неавторизованный доступ
+  }
 });
 
 
 
-
-
-
-
-
-
 //Роуты страниц
-
 //1. Бронирования
 app.get('/booking', (req, res) => {
   res.render('booking');
